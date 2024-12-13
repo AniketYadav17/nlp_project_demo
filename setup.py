13a2902950e8f1a0b9bc9a2b10641d107e10cebd
Aniket <41797865+AniketYadav17@users.noreply.github.com>
2024-12-13 01:37:49 +0000

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech_classification",
+    version="0.0.1",
+    author="Aniket",
+    author_email="",
+    packages=find_packages(),
+    install_requires=[]
+)
